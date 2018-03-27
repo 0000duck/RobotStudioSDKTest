08d9e6a1f4ec177d45fe8a4210d338421a10e913
nunezarn <nunezarn@cse.msu.edu>
2018-03-27 18:25:30 -0400

    First Commit

---
+++ AddInMain/Class1.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+//using ABB.Robotics.Math;
+//using ABB.Robotics.RobotStudio;
+//using ABB.Robotics.RobotStudio.Environment;
+//using ABB.Robotics.RobotStudio.Stations;
+
+//	To activate this Add-in you have to copy AddInMain.rsaddin to the Add-In directory,
+//  typically C:\Program Files (x86)\Common Files\ABB Industrial IT\Robotics IT\RobotStudio\AddIns
+
+namespace AddInMain
+{
+    public class Class1
+    {
+        private const string introStr = "Hello, I am a RobotStudio Add-In!";
+
+
+        // This is the entry point which will be called when the Add-in is loaded
+        public static void AddinMain()
+        {
+            ABB.Robotics.RobotStudio.Logger.AddMessage(new ABB.Robotics.RobotStudio.LogMessage(introStr));
+        }
+    }
+}
